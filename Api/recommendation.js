f54492fb1da9898a65792899ce78d3124f8f3daa
TS <423183@cognizant.com>
2017-05-17 20:16:41 +0530

    added socket authentication and change style

--- Api/recommendation.js
+++ Api/recommendation.js
@@ -1,13 +1,35 @@
 var sockets = require('../socket/socket');
+var jwt = require('jsonwebtoken');
 
+
+var impObject = {
+    'jwtSecret': 'mockcontactcenter'
+};
 module.exports.controller = function (apiRouts) {
-    apiRouts.post("/sendRecommendation", function (req, resp) { 
+    apiRouts.post("/sendRecommendation", function (req, resp) {
         var skt = sockets.GetSocket(req.body.AgentId);
-        if (req.body && req.body.AgentId && skt) {
-            skt.emit('chat message', req.body.Message);
-            resp.json({ message: 'Success. Message Sent'});
+        var token = req.query.token;
+        if (token) {
+            jwt.verify(token, impObject.jwtSecret, function (err, decoded) {
+                if (err) {
+                    return resp.json({ success: false, message: 'Failed to authenticate token.' });
+                    console.log('error');
+                } else {
+                    // if everything is good, save to request for use in other routes
+                    req.decoded = decoded;
+                    if (req.body && req.body.AgentId && skt) {
+                        skt.emit('chat message', req.body.Message);
+                        resp.json({ message: 'Success. Message Sent' });
+                    } else {
+                        resp.json({ message: 'Error. Message Not Sent!' });
+                    }
+                }
+            });
         } else {
-            resp.json({ message: 'Error. Message Not Sent!'});
+            return resp.status(403).send({
+                success: false,
+                message: 'No token provided.'
+            });
         }
     });
 }
