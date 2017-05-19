aa4766aa311f62d0547d3bba12b285b43e3fadf5
Dhrisya TS <dhrisya.ts@cognizant.com>
2017-05-19 11:33:13 +0530

    NLP Api integration and client interface (#8)

--- Api/recommendation.js
+++ Api/recommendation.js
@@ -1,10 +1,12 @@
 var sockets = require('../socket/socket');
 var jwt = require('jsonwebtoken');
-
+var Client = require('node-rest-client').Client;
+var client = new Client();
 
 var impObject = {
     'jwtSecret': 'mockcontactcenter'
 };
+var authenticatedToken;
 module.exports.controller = function (apiRouts) {
 
     apiRouts.post("/sendRecommendation", function (req, resp) {
@@ -36,5 +38,37 @@ module.exports.controller = function (apiRouts) {
             });
         }
     });
+
+    apiRouts.post("/processMessage", function (req, resp) {
+        var arg = {
+            result: { "message": req.body.message, "clientId": req.body.clientId, "connectId": req.body.clientId },
+            headers: { "Content-Type": "application/json"}
+        };
+
+        client.post("https://nlp-mock-api.herokuapp.com/processMessage", arg, function (result, response) {
+            console.log(response.statusCode);
+            if (response.statusCode == 401) {
+                var args = {
+                    data: { "appId": 1 },
+                    headers: { "Content-Type": "application/json" }
+                };
+                client.post("https://nlp-mock-api.herokuapp.com/authenticate", args, function (data, res) {
+                     console.log(data["token"]);
+                    var arg1 = {
+                        data: { "message": req.body.message, "clientId": req.body.clientId, "connectId": req.body.clientId },
+                        headers: { "Content-Type": "application/json", "Authorization": data["token"] }
+                    };
+                    client.post("https://nlp-mock-api.herokuapp.com/processMessage", arg1, function (authReq, authres) {
+                        console.log(authReq);
+                    });
+
+                });
+            }
+        });
+
+        resp.json({ message: 'Test' });
+    });
+
+
 }
 
---
+++ public/js/agent.js
@@ -0,0 +1,50 @@
+
+
+function field_focus(field, email) {
+    if (field.value == email) {
+        field.value = '';
+    }
+}
+function field_blur(field, email) {
+    if (field.value == '') {
+        field.value = email;
+    }
+}
+$(document).ready(function () {
+    $('.box').hide().fadeIn(1000);
+});
+
+$('#connectButtonId').click(function (event) {
+    event.preventDefault();
+    window.open("http://localhost:8080", '',
+        'width=450,height=600,scrollbars=yes,menubar=yes,status=yes,resizable=no,directories=false,location=false,left=0,top=0'
+    );
+});
+
+$('#sendButtonId').click(function () {
+    var clientId = document.getElementById("clientId").value;
+    var clientMessageId = document.getElementById("clientMessageId").value;
+
+    $.post("/api/processMessage",
+                {
+            clientId: clientId,
+            message:  clientMessageId
+               },
+                function  (data,  status)  {
+                  alert(data.message);  
+                });
+
+
+
+    //     $.ajax({
+    //     url : "/ajaxRoute",
+    //     type: "POST",
+    //     dataType:'json',
+    //     data : appId,
+    //     success: function(data){
+    //         console.log(data.msg); // 'OK'
+    //     },
+    // });
+
+
+});
